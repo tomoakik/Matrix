45071305d763c9e7e4d423bfee375598442ab1dd
50616013 <50616013@50616013-pc>
2017-11-06 18:20:31 +0900

    matrix

--- src/matrix1/Matrix1_main.java
+++ src/matrix1/Matrix1_main.java
@@ -7,11 +7,13 @@ public class Matrix1_main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-				double a[][] = {{0,1},{2,0},{0,3}};
-				double b[][] = {{1,-1,2},{3,0,4}};
-				Matrix1_lib mlib = new Matrix1_lib(a,b);
-				System.out.println("Product = "+Arrays.deepToString(mlib.getProduct()));
-				System.out.println("Transpose = "+Arrays.deepToString(mlib.getTranspose(b)));
+				double a[][] = {{1,2},{-1,1},};
+				double b[][] = {{3,-1},{2,-1}};
+				double c[][] = {{3,1,1,2},{5,1,3,4},{2,0,1,0},{1,3,2,1}};
+				Matrix1_lib mlib = new Matrix1_lib();
+				System.out.println("Product = "+Arrays.deepToString(mlib.getProduct(a,b)));
+				System.out.println("Cofactor = "+(mlib.getCofactor(c)));
+				System.out.println("Inverse ="+Arrays.deepToString(mlib.getInverse(c)));
 
 			}
 
